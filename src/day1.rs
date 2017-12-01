3e434818dab1ba397fc9902eb9826f1361892b69
ProgramFOX <programfox@hotmail.be>
2017-12-01 17:33:14 +0100

    Use modulo for part 1

--- src/day1.rs
+++ src/day1.rs
@@ -2,7 +2,7 @@ pub fn part1(input: String) -> String {
     let mut sum = 0;
 
     for i in 0..input.len() {
-        let next_index = if i == input.len() - 1 { 0 } else { i + 1 };
+        let next_index = (i + 1) % input.len();
         let current_char = input.chars().nth(i).unwrap();
         if current_char == input.chars().nth(next_index).unwrap() {
             sum += current_char.to_digit(10).unwrap();
