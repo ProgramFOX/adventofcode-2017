4d45239b0d32f77164da594d3c81c73c1aeee848
ProgramFOX <programfox@hotmail.be>
2017-12-02 16:04:33 +0100

    Implement day 2 part 1

--- src/day2.rs
+++ src/day2.rs
@@ -1,5 +1,23 @@
+use std::fs;
+use std::io;
+use std::io::BufRead;
+
 pub fn part1(input: &str) -> String {
-    String::from("not yet implemented")
+    let file = fs::File::open(input).expect("Failed to open file");
+    let reader = io::BufReader::new(&file);
+
+    let mut sum = 0;
+    for l in reader.lines() {
+        let line = l.expect("Failed to read from file");
+        let integers: Vec<i32> = line.split("\t")
+            .map(|x| x.parse().expect("Input file contained non-integers"))
+            .collect();
+        let min = integers.iter().min().expect("Failed to find min value");
+        let max = integers.iter().max().expect("Failed to find max value");
+        sum += max - min;
+    }
+
+    sum.to_string()
 }
 
 pub fn part2(input: &str) -> String {
