1edf129413a709b6733b7721cedab047f3c76d48
ProgramFOX <programfox@hotmail.be>
2017-12-02 16:10:31 +0100

    Implement day 2 part 2

--- src/day2.rs
+++ src/day2.rs
@@ -21,5 +21,34 @@ pub fn part1(input: &str) -> String {
 }
 
 pub fn part2(input: &str) -> String {
-    String::from("not yet implemented")
+    let file = fs::File::open(input).expect("Failed to open file");
+    let reader = io::BufReader::new(&file);
+
+    let mut sum = 0;
+    for l in reader.lines() {
+        let line = l.expect("Failed to read from file");
+        let integers: Vec<i32> = line.split("\t")
+            .map(|x| x.parse().expect("Input file contained non-integers"))
+            .collect();
+        let mut found = false;
+        for i in 0..(integers.len() - 1) {
+            for j in (i + 1)..integers.len() {
+                if integers[i] % integers[j] == 0 {
+                    sum += integers[i] / integers[j];
+                    found = true;
+                } else if integers[j] % integers[i] == 0 {
+                    sum += integers[j] / integers[i];
+                    found = true;
+                }
+                if found {
+                    break;
+                }
+            }
+            if found {
+                break;
+            }
+        }
+    }
+
+    sum.to_string()
 }
