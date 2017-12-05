6e3e4238483fa0e81c2ec0d85b42e3605e942cd2
ProgramFOX <programfox@hotmail.be>
2017-12-05 17:49:41 +0100

    Day 5

--- src/day5.rs
+++ src/day5.rs
@@ -1,7 +1,51 @@
+use std::io;
+use std::fs;
+use std::io::BufRead;
+
 pub fn part1(input: &str) -> String {
-    String::from("not yet implemented")
+    let file = fs::File::open(input).expect("Failed to open file");
+    let reader = io::BufReader::new(&file);
+
+    let mut jumps: Vec<isize> = vec![];
+    for l in reader.lines() {
+        let line = l.expect("Failed to read from file");
+        let line = line.trim();
+        jumps.push(line.parse().expect("Failed to convert line to integer"));
+    }
+
+    let mut index: isize = 0;
+    let mut steps = 0;
+    loop {
+        steps += 1;
+        let jump = jumps[index as usize];
+        jumps[index as usize] += 1;
+        index += jump;
+        if index < 0 || index >= jumps.len() as isize {
+            return steps.to_string();
+        }
+    }
 }
 
 pub fn part2(input: &str) -> String {
-    String::from("not yet implemented")
+    let file = fs::File::open(input).expect("Failed to open file");
+    let reader = io::BufReader::new(&file);
+
+    let mut jumps: Vec<isize> = vec![];
+    for l in reader.lines() {
+        let line = l.expect("Failed to read from file");
+        let line = line.trim();
+        jumps.push(line.parse().expect("Failed to convert line to integer"));
+    }
+
+    let mut index: isize = 0;
+    let mut steps = 0;
+    loop {
+        steps += 1;
+        let jump = jumps[index as usize];
+        jumps[index as usize] += if jump < 3 { 1 } else { -1 };
+        index += jump;
+        if index < 0 || index >= jumps.len() as isize {
+            return steps.to_string();
+        }
+    }
 }
